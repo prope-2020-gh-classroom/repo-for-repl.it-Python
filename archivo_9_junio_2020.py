e03fc43651af06ba2737e9dbfdb67bc40e55be7a
Erick Palacios Moreno <palmoreck@gmail.com>
2020-06-11 01:14:47 +0000

    adding 10 june class

--- archivo_9_junio_2020.py
+++ archivo_9_junio_2020.py
@@ -1,16 +1,18 @@
 import numpy as np
 
 a = np.array([1,2,3])
-b = np.arrya([1,1,1])
-c = np.
+b = np.array([1,1,1])
+#c = np.
 
 def mi_funcion(a,b):
   return 0
   
-mi_funcion()
+mi_funcion(a,b)
 print("Variable: ", a)
 
 
 
  
+def otra_funcion():
+  return("Hola")
 
---
+++ clase_10_junio_2020.py
@@ -0,0 +1,61 @@
+
+string = "Un día soleado como hoy :)"
+
+string2 = "así es!"
+
+print("Seleccionamos string:")
+print(string)
+print("De la posición 1 a la posición 5:")
+print(string[0:5])
+
+
+string3 = "".join([string, "\n", string2])
+
+print("Concatenamos string:")
+print(string)
+print("con string2:")
+print(string2)
+
+print(string3)
+
+
+string4 = """Un día soleado como hoy :)
+así es!
+"""
+
+print(string4)
+
+print("Utilizando splitlines:")
+l = string4.splitlines()
+print(l)
+
+print("Utilizando split:")
+print(string4.split())
+
+print("Usamos un ciclo for:")
+k = 0
+
+for linea in string4.splitlines():
+  print(linea)
+  print(k)
+  k+=1 #k = k + 1
+
+print("lista ejemplo:")
+l2 = [1,2,3]
+
+for i in l2:
+  print(i)
+
+
+
+#string5="1 2 3 4 5 6 7 8"
+#print(string5.split())
+
+
+
+
+
+
+
+
+
