177abc4033397268cdf2351a4ec737a79bd2dbf4
Erick Palacios Moreno <palmoreck@gmail.com>
2020-06-13 18:52:22 +0000

    adding some lines to split sections

--- clase_10_junio_2020.py
+++ clase_10_junio_2020.py
@@ -1,5 +1,5 @@
 
-string = "Un día soleado como hoy :)"
+string = "xxx Un día soleado como hoy :)"
 
 string2 = "así es!"
 
@@ -11,6 +11,7 @@ print(string[0:5])
 
 string3 = "".join([string, "\n", string2])
 
+print("-"*20)
 print("Concatenamos string:")
 print(string)
 print("con string2:")
@@ -18,6 +19,9 @@ print(string2)
 
 print(string3)
 
+print("-"*20)
+
+print("Usamos tres dobles comillas y saltos de línea:")
 
 string4 = """Un día soleado como hoy :)
 así es!
@@ -25,12 +29,18 @@ así es!
 
 print(string4)
 
+print("-"*20)
+
 print("Utilizando splitlines:")
 l = string4.splitlines()
 print(l)
 
+print("-"*20)
+
 print("Utilizando split:")
 print(string4.split())
+
+print("-"*20)
 
 print("Usamos un ciclo for:")
 k = 0
@@ -39,6 +49,8 @@ for linea in string4.splitlines():
   print(linea)
   print(k)
   k+=1 #k = k + 1
+
+print("-"*20)
 
 print("lista ejemplo:")
 l2 = [1,2,3]
@@ -57,5 +69,5 @@ for i in l2:
 
 
 
-
+ 
 
