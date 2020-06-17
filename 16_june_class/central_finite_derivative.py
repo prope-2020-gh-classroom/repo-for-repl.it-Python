728e8ba5e4f53c4fbc9b8c0166fdd1adf614fe2d
Erick Palacios Moreno <palmoreck@gmail.com>
2020-06-17 13:58:55 +0000

    fixing identations

--- 16_june_class/central_finite_derivative.py
+++ 16_june_class/central_finite_derivative.py
@@ -1,31 +1,30 @@
-
 def approx_first_derivative(f,x,h):
-  """
-  Numerical differentiation by finite differences. Uses central point formula
-  to approximate first derivative of function.
-  Args:
-      f (function): function definition.
-      x (float): point where first derivative will be approximated
-      h (float): step size for central differences. Tipically less than 1
-  Returns:
-      df (float): approximation to first_derivative.
-  """
-  df = (f(x+h) - f(x-h))/(2.0*h)
-  return df
+    """
+    Numerical differentiation by finite differences. Uses central point formula
+    to approximate first derivative of function.
+    Args:
+        f (function): function definition.
+        x (float): point where first derivative will be approximated
+        h (float): step size for central differences. Tipically less than 1
+    Returns:
+        df (float): approximation to first_derivative.
+    """
+    df = (f(x+h) - f(x-h))/(2.0*h)
+    return df
 
 def approx_second_derivative(f,x,h):
-  """
-  Numerical differentiation by finite differences. Uses central point formula
-  to approximate second derivative of function.
-  Args:
-      f (function): function definition.
-      x (float): point where second derivative will be approximated
-      h (float): step size for central differences. Tipically less than 1
-  Returns:
-      ddf (float): approximation to second_derivative.
-  """
-  ddf =(f(x+h) - 2.0*f(x) + f(x-h))/h**2
-  return ddf
+    """
+    Numerical differentiation by finite differences. Uses central point formula
+    to approximate second derivative of function.
+    Args:
+        f (function): function definition.
+        x (float): point where second derivative will be approximated
+        h (float): step size for central differences. Tipically less than 1
+    Returns:
+        ddf (float): approximation to second_derivative.
+    """
+    ddf =(f(x+h) - 2.0*f(x) + f(x-h))/h**2
+    return ddf
 
 def main_function():
     """
